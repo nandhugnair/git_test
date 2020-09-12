5cd803d474a80264ac7cbdab1d6ccacaed4307c4
Nandhu G Nair <66883683+nandhugnair@users.noreply.github.com>
2020-09-13 01:04:00 +0530

    Update DemoApplication.java

--- docker-demo/src/main/java/com/example/demo/DemoApplication.java
+++ docker-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	public static void main(final String[] args) {
 		final ConfigurableApplicationContext configurableApplicationContext = SpringApplication
-				.run(SpringMain.class, args);
+				.run(DemoApplication.class, args);
 		
 		 
 	}
